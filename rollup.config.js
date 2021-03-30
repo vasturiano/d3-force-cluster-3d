f826431af60a626e1dc349729d3b728134edb21e
vasco <vastur@gmail.com>
2021-03-31 00:38:46 +0100

    Initial commit

---
+++ rollup.config.js
@@ -0,0 +1,55 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonJs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import { terser } from 'rollup-plugin-terser';
+import { name, homepage, version, dependencies } from './package.json';
+
+const umdConf = {
+  format: 'umd',
+  extend: true,
+  name: 'd3',
+  banner: `// Version ${version} ${name} - ${homepage}`
+};
+
+export default [
+  { // UMD
+    input: 'src/index.js',
+    output: [
+      {
+        ...umdConf,
+        file: `dist/${name}.js`,
+        sourcemap: true
+      },
+      { // minify
+        ...umdConf,
+        file: `dist/${name}.min.js`,
+        plugins: [terser({
+          output: { comments: '/Version/' }
+        })]
+      }
+    ],
+    plugins: [
+      resolve(),
+      commonJs(),
+      babel({ exclude: 'node_modules/**' })
+    ]
+  },
+  { // commonJs and ES modules
+    input: 'src/cluster.js',
+    output: [
+      {
+        format: 'cjs',
+        file: `dist/${name}.common.js`,
+        exports: 'auto'
+      },
+      {
+        format: 'es',
+        file: `dist/${name}.module.js`
+      }
+    ],
+    external: Object.keys(dependencies),
+    plugins: [
+      babel()
+    ]
+  },
+];
